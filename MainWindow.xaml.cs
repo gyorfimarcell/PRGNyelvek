a231760a578aeb986d4719aa847508229eb07ba3
Györfi Marcell <44555742+gyorfimarcell@users.noreply.github.com>
2023-03-02 10:23:39 +0100

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp6
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void btnRogzit_Click(object sender, RoutedEventArgs e)
+        {
+            string neve = tbNeve.Text;
+
+            int fordito = 0;
+            if (rbFordito.IsChecked == true)
+            {
+                fordito = 1;
+            }
+
+            int nyelvcsalad = cbNyelvcsalad.SelectedIndex + 1;
+
+            string megjelenesEve = tbMegjelenes.Text;
+
+            int magasSzintu = 0;
+            if (rbMagas.IsChecked == true)
+            {
+                magasSzintu = 1;
+            }
+
+            int nepszeruseg = Convert.ToInt32(slNepszeruseg.Value);
+
+
+            string sor = $"{neve};{fordito};{nyelvcsalad};{megjelenesEve};{magasSzintu};{nepszeruseg}";
+
+            StreamWriter iro = new StreamWriter("nyelvek.txt", true);
+            iro.WriteLine(sor);
+            iro.Close();
+        }
+    }
+}
---
+++ Nyelv.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp6
+{
+    public enum Nyelvcsaladok { moduláris, strukturált, funkcionális, objektumorientált };
+
+    public class Nyelv
+    {
+        string neve;
+        bool forditosE;
+        Nyelvcsaladok nyelvcsalad;
+        int megjelenesEve;
+        bool alacsonySzintuE;
+        int nepszeruseg2022;
+
+        public Nyelv(string neve,
+                     bool forditosE,
+                     Nyelvcsaladok nyelvcsalad,
+                     int megjelenesEve,
+                     bool alacsonySzintuE,
+                     int nepszeruseg2022)
+        {
+            this.neve = neve;
+            this.forditosE = forditosE;
+            this.nyelvcsalad = nyelvcsalad;
+            this.megjelenesEve = megjelenesEve;
+            this.alacsonySzintuE = alacsonySzintuE;
+            this.nepszeruseg2022 = nepszeruseg2022;
+        }
+
+        public string Neve { get => neve; set => neve = value; }
+        public bool ForditosE { get => forditosE; set => forditosE = value; }
+        public Nyelvcsaladok Nyelvcsalad { get => nyelvcsalad; set => nyelvcsalad = value; }
+        public int MegjelenesEve { get => megjelenesEve; set => megjelenesEve = value; }
+        public bool AlacsonySzintuE { get => alacsonySzintuE; set => alacsonySzintuE = value; }
+        public int Nepszeruseg2022 { get => nepszeruseg2022; set => nepszeruseg2022 = value; }
+
+        /// <summary>
+        ///
+        /// </summary>
+        /// <returns>Ha a legnépszerűbb 3-ba tartozik</returns>
+        public bool NepszeruE() {
+            return this.nepszeruseg2022 <= 3;
+        }
+    }
+}
